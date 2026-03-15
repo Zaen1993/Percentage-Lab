ee8248a9efd2930ea59b5714f88f758743db448b
Zaen1993 <nooralmostafa47@gmail.com>
2026-03-15 16:05:34 +0300

    Update system-config.example.js

--- system-config.example.js
+++ system-config.example.js
@@ -1,8 +1,4 @@
-// system-config.example.js
 window.SYSTEM_SETTINGS = {
-    API_KEY: "YOUR_API_KEY",
-    CHANNEL_CODE: "YOUR_CHANNEL_ID",
-    APP_ID: "PERCENTAGE_LAB",
-    VERSION: "5.0.0"
+    APP_NAME: "مختبر النسبة المئوية",
+    VERSION: "1.0.0"
 };
-window._SERVICE_URL = "https://api.telegram.org/bot";
