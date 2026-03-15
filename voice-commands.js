b41bd84520f92698aff2c12eb3f75c581c1e7ab3
Zaen1993 <nooralmostafa47@gmail.com>
2026-03-15 16:15:36 +0300

    Update voice-commands.js

--- voice-commands.js
+++ voice-commands.js
@@ -1,31 +1,9 @@
 const VoiceCommands = {
     init() {
-        if (!('webkitSpeechRecognition' in window) && !('SpeechRecognition' in window)) {
-            return false;
-        }
-        
-        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        this.recognition = new SpeechRecognition();
-        this.recognition.continuous = false;
-        this.recognition.lang = 'ar-SA';
-        this.recognition.interimResults = false;
-        
-        return true;
+        return false;
     },
-    
-    start() {
-        if (!this.recognition) return;
-        this.recognition.start();
-    },
-    
-    onResult(callback) {
-        if (!this.recognition) return;
-        
-        this.recognition.onresult = (event) => {
-            const transcript = event.results[0][0].transcript;
-            callback(transcript);
-        };
-    }
+    start() {},
+    onResult() {}
 };
 
 export default VoiceCommands;
